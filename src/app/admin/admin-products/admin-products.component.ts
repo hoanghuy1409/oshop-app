e801ea1e9f4f7ec6f0cbc68f30e6bbfe8247e3f4
Hoang Huy <hoanghuy.gg96@gmail.com>
2020-01-16 22:53:54 +0700

    filter product

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,37 @@
 import { ProductService } from "./../../product.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-admin-products",
   templateUrl: "./admin-products.component.html",
   styleUrls: ["./admin-products.component.css"]
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
+
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAdd();
+    this.subscription = this.productService
+      .getAdd()
+      .subscribe(
+        products => (this.filteredProducts = this.products = products)
+      );
   }
 
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(p =>
+          p.payload
+            .val()
+            .title.toLowerCase()
+            .includes(query.toLowerCase())
+        )
+      : this.products;
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
   ngOnInit() {}
 }
